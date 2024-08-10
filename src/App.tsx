0d97abb64cb011a2cef7ce439bba9db2f4e62418
Maxim Sur <makarka16@gmail.com>
2024-08-10 22:56:29 +0300

    Add: load products with request

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./index.css"
 import Product from './components/Product';
-import { products } from './data/products';
-
+import { IProduct } from './models';
+import axios from 'axios';
 
 function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([])
+
+
+  useEffect(() => {
+    loadProducts();
+  }, [])
+
+  async function loadProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+    setProducts(response.data);
+  }
+
   return (
     <div className="flex items-center flex-col gap-5">
-      {products.map((product) => {
+      {products && products.map((product) => {
         return <Product product={product}/>
       })}
     </div>
