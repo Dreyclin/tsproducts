4859b9f5d597dd60d11f4a8477378956ba366362
Maxim Sur <makarka16@gmail.com>
2024-08-11 13:23:23 +0300

    Add: show details button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { IProduct } from "../models";
 
 interface ProductProps {
     product: IProduct
 }
 
-const Product: React.FC<ProductProps> = ({product}) => {
-    return(
-        <div className="flex py-3 px-3 flex-col items-center border-slate-200 border-2 w-2/4 rounded-md my-5">
-            <img src={product.image} alt="" className="w-64"/>
+const Product: React.FC<ProductProps> = ({ product }) => {
+
+    const [desc, setDesc] = useState<boolean>(false);
+
+    return (
+        <div className="flex py-3 px-3 flex-col items-center border-slate-200 border-2 w-1/4 rounded-md my-5">
+            <img src={product.image} alt="" className="w-32" />
             <p className="text-2xl">{product.title}</p>
-            <p className="text-xl">{product.description}</p>
+            <p className="font-bold text-xl">{product.price}$</p>
+            {desc && <p className="text-xl">{product.description}</p>}
+            <button className="bg-blue-500 py-4 px-6 font-bold text-xl text-slate-50 hover:bg-blue-700 my-3" onClick={() => setDesc(!desc)}>Show details</button>
         </div>
     )
 }
